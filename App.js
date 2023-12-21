c0d31a199cc61a1ba0aee0e6b1ce3c5315ef2e3a
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-21 15:11:01 +0300

    Refactor App.js to add goal functionality

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View,Button } from "react-native";
+import React, { useState } from "react";
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
+  const [goalList, setGoalList] = useState([]);
+  const  [isModalVisible, setIsModalVisible] = useState(false);
+
+
+  const startAddGoalHandler = () => {
+    setIsModalVisible(true);
+  }
+
+  const addGoalHandler = (enteredGoal) => {
+    setGoalList((currentGoals) => [
+      {
+        text: enteredGoal,
+        id: Math.random().toString(),
+      },
+      ...currentGoals,
+    ]);
+  };
+
+
+    const removeGoalHandler = (goalId) => {
+      setGoalList((currentGoals) => {
+        return currentGoals.filter((goal) => goal.id !== goalId);
+      });
+    }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <Button color="#311b6b" title="Add New Goal" onPress={startAddGoalHandler} />
+      {isModalVisible&&(<GoalInput onAddGoal={addGoalHandler} visible={isModalVisible} onCancel={()=>setIsModalVisible(false)}/> )}
+      <GoalItem goalList={goalList} onRemoveGoal={removeGoalHandler}/>
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "start",
+    marginTop: 50,
+    backgroundColor: "#fff",
   },
 });
